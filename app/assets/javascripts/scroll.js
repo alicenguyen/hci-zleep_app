d1fc71ec6b21cfe8f139b89da6813b2b28a7b399
Jake Pham <jakepham90@gmail.com>
2014-03-06 08:59:54 -0800

    Add scrollup

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 100){
+            $('#goTop').stop().animate({
+                bottom: '20px'    
+                }, 500);
+        }
+        else{
+            $('#goTop').stop().animate({
+               bottom: '20px'    
+            }, 500);
+        }
+    });
+    $('#goTop').click(function() {
+        $('html, body').stop().animate({
+           scrollTop: 0
+        }, 500, function() {
+           $('#goTop').stop().animate({
+               bottom: '20px'    
+           }, 500);
+        });
+    });
+});    
